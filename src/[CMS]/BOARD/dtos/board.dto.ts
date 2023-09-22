98417ba392807fc38707b9ebc172ccdc725c19d4
eikjae <eikjae@naver.com>
2023-09-22 11:58:02 +0900

    FIX: 포트 10040번으로 변경

--- src/[CMS]/BOARD/dtos/board.dto.ts
+++ src/[CMS]/BOARD/dtos/board.dto.ts
@@ -1,12 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
 export class InquireDto {
+  @ApiProperty({ example: '홍길동', description: 'name', required: true })
   @IsString()
   name: string;
 
+  @ApiProperty({ example: '01010101010', description: 'phone', required: true })
   @IsString()
   phone: string;
 
+  @ApiProperty({
+    example: '제휴를 원합니다.',
+    description: 'content',
+    required: true,
+  })
   @IsString()
   content: string;
 }
