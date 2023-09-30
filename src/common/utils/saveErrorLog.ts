04d08fc9b1cc206a9c6f05a93545a74c84b4bc1c
eikjae <eikjae@naver.com>
2023-09-30 20:28:21 +0900

    FEAT: errorlog url 변경

--- src/common/utils/saveErrorLog.ts
+++ src/common/utils/saveErrorLog.ts
@@ -9,8 +9,7 @@ export const saveErrorLog = async (
   await axios({
     method: 'post',
     //! 현재는 하드코딩. url 처리 고민하기 (현재 실행중인 url을 사용할 수 있도록)
-    // url: 'http://localhost:10040/log/error',
-    url: 'https://dev25backend.givemeprice.co.kr/log/error',
+    url: 'http://localhost:10040/log/error',
     data: {
       status_code,
       method,
