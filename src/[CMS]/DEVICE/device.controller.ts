fbcbb16987825809bf286567e587ef1a86b975d5
eikjae <eikjae@naver.com>
2023-10-16 12:00:31 +0900

    FEAT: device API 관련 swagger 설명 추가

--- src/[CMS]/DEVICE/device.controller.ts
+++ src/[CMS]/DEVICE/device.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { DeviceService } from './device.serivce';
 import { DeviceIdDto } from './dtos/deviceId.dto';
 import { fcmpushAllDto, fcmpushDto } from './dtos/fcmpush.dto';
@@ -10,6 +10,7 @@ export class DeviceController {
   constructor(private readonly deviceService: DeviceService) {}
 
   //! (특정 기기에) 푸시 전송하기
+  @ApiOperation({ summary: '(특정 기기에) 푸시 전송하기' })
   @Post('fcmpush')
   async sendFcmpush(@Body() body: fcmpushDto) {
     const user = await this.deviceService.sendFcmpush(body);
@@ -17,6 +18,7 @@ export class DeviceController {
   }
 
   //! (전체 기기에) 푸시 전송하기
+  @ApiOperation({ summary: '(전체 기기에) 푸시 전송하기' })
   @Post('fcmpush/all')
   async sendFcmpushAll(@Body() body: fcmpushAllDto) {
     const user = await this.deviceService.sendFcmpushAll(body);
@@ -24,12 +26,14 @@ export class DeviceController {
   }
 
   //! 기기 아이디 저장하기
+  @ApiOperation({ summary: '기기 아이디 저장하기' })
   @Post('deviceid')
   async setDeviceId(@Body() body: DeviceIdDto) {
     return await this.deviceService.setDeviceId(body);
   }
 
   //! (모든) 기기 정보 가져오기
+  @ApiOperation({ summary: '(모든) 기기 정보 가져오기' })
   @Get('deviceid')
   async getAll(@Query('take') take?: number, @Query('page') page?: number) {
     return await this.deviceService.getAll(take, page);
