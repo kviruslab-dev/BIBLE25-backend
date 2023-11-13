1de7d6bcb017ae1bfd05ca65fd169dc7da20097b
eikjae <eikjae@naver.com>
2023-11-13 10:26:34 +0900

    FIX: 슬랙 알림 로직 수정 (GET / 요청 알림 해제)

--- src/apex/LOG/log.service.ts
+++ src/apex/LOG/log.service.ts
@@ -10,25 +10,6 @@ export class LogService {
   async SaveError(data: ErrorLogDto) {
     const { status_code, method, url, error } = data;
 
-    //! 3일 내에 같은 에러가 발생한 적이 있는지 확인한다.
-    // const findOneCondition = {
-    //   select: 'id, count',
-    //   table: 'error_log',
-    //   where: `status_code=${status_code} and method='${method}' and url='${url}' and error='${error}' and DATEDIFF(NOW(), created_at) <= 3`,
-    // };
-
-    // const errorLog = await this.queryRunnerService.findOne(findOneCondition);
-
-    //! 3일 내에 발생하지 않았던 에러인 경우, 슬랙봇을 통해 개발팀에 알린다.
-    // if (!errorLog) {
-    sendMessageToSlack(`
-      🚨🚨🚨 STATUS CODE : ${status_code} 🚨🚨🚨
-      오류 메세지 : Cannot ${method} ${url},
-      ${error}
-      `);
-    // }
-
-    //! 에러 내용을 저장한다.
     const condition = {
       table: 'error_log',
       columns: `status_code, method, url, error, count`,
@@ -36,6 +17,15 @@ export class LogService {
     };
 
     await this.queryRunnerService.insert(condition);
+
+    if (method === 'GET' && url === '/') return;
+
+    sendMessageToSlack(`
+    🚨 STATUS CODE : ${status_code} 🚨
+    오류 메세지 : Cannot ${method} ${url},
+    ${error}
+    `);
+
     return;
   }
 }
