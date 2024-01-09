1d15a2b8b01beb319966f7c42ccf2555e6062b54
eikjae <eikjae@naver.com>
2024-01-09 10:51:00 +0900

    FIX: market_item 데이터 형식 백엔드에서 제어

--- src/apex/PRODUCT/product.service.ts
+++ src/apex/PRODUCT/product.service.ts
@@ -6,9 +6,26 @@ export class ProductService {
   constructor(private readonly queryRunnerService: QueryRunnerService) {}
 
   async find(condition: any) {
-    const data01 = await this.queryRunnerService.findAndCount(condition(1));
-    const data02 = await this.queryRunnerService.findAndCount(condition(2));
+    const temp01 = await this.queryRunnerService.findAndCount(condition(1));
+    const temp02 = await this.queryRunnerService.findAndCount(condition(2));
 
-    return { 1: data01.list, 2: data02.list };
+    const data01 = temp01.list.map((v: any) => {
+      v.dc += '%할인';
+      return v;
+    });
+
+    const data02 = temp02.list.map((v: any, i: number) => {
+      //! 두번째 줄 2개 상품은 렌탈로 사용하지 않을 예정
+      // if (i < 2) {
+      //   v.dc += '%할인';
+      //   return v;
+      // }
+
+      v.money = `월 ${v.money}`;
+      v.dc += '개월';
+      return v;
+    });
+
+    return { 1: data01, 2: data02 };
   }
 }
