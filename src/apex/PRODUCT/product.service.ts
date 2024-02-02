49c4a400a80418936d4c09dbd99862d0ebeed18d
eikjae <eikjae@naver.com>
2024-02-03 01:24:29 +0900

    fix: 후원상품 두번째 줄 두번째 까지 상품으로 변경

--- src/apex/PRODUCT/product.service.ts
+++ src/apex/PRODUCT/product.service.ts
@@ -16,7 +16,7 @@ export class ProductService {
     });
 
     const data02 = temp02.list.map((v: any, i: number) => {
-      if (i < 1) {
+      if (i < 2) {
         v.money = v.money.toLocaleString('ko-KR');
         v.dc += '%할인';
         return v;
