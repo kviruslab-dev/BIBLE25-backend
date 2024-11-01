8a6524b909e69fd3e5b95e218bb69a72d15bf259
eikjae <eikjae@naver.com>
2024-11-01 10:08:28 +0900

    fix : 상품 정보 변경

--- src/apex/PRODUCT/product.service.ts
+++ src/apex/PRODUCT/product.service.ts
@@ -16,7 +16,7 @@ export class ProductService {
     });
     // 바이블25 후원상품 둘째줄
     const data02 = temp02.list.map((v: any, i: number) => {
-      if (i == 0 || i == 2) {
+      if (i == 0 || i == 1 || i == 2) {
         v.money = v.money.toLocaleString('ko-KR');
         v.dc += '%할인';
         return v;
